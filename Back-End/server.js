487515b0ca0fe951d15e40b91be0723f82e30f97
David <dajohnson1998@gmail.com>
2019-02-04 16:37:57 -0500

    auth setup on the backend

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,51 +1,97 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { PORT, MONGODB_URI } = require('./config');
+const morgan = require('morgan');
+const passport = require('passport');
+const { PORT, DATABASE_URL } = require('./config');
 const charactersRouter = require('./routes/characters')
+
+const {localStrategy, jwtStrategy} = require('./strategies')
 const app = express();
 
- app.use(express.static('public'));
- 
- app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+// Here we use destructuring assignment with renaming so the two variables
+// called router (from ./users and ./auth) have different names
+// For example:
+// const actorSurnames = { james: "Stewart", robert: "De Niro" };
+// const { james: jimmy, robert: bobby } = actorSurnames;
+// console.log(jimmy); // Stewart - the variable name is jimmy, not james
+// console.log(bobby); // De Niro - the variable name is bobby, not robert
+
+const { router: usersRouter } = require('./routes/users');
+const { router: authRouter } = require('./routes/auth')
+mongoose.Promise = global.Promise;
+
+
+app.use(morgan('common'));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
   next();
 });
 
+passport.use(localStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+app.use('/api/auth/', authRouter);
 app.use('/characters', charactersRouter);
 
-app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+const jwtAuth = passport.authenticate('jwt', { session: false });
+
+// A protected endpoint which needs a valid JWT to access it
+app.get('/api/protected', jwtAuth, (req, res) => {
+  return res.json({
+    data: 'rosebud'
+  });
 });
 
-
-app.use((err, req, res, next) => {
-  if (err.status) {
-    const errBody = Object.assign({}, err, { message: err.message });
-    res.status(err.status).json(errBody);
-  } else {
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
+app.use('*', (req, res) => {
+  return res.status(404).json({ message: 'Not Found' });
 });
 
-if (require.main === module) {
+// Referenced by both runServer and closeServer. closeServer
+// assumes runServer has run and set `server` to a server object
+let server;
 
-  mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex : true })
-    .then(instance => {
-      const conn = instance.connections[0];
-      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
-    })
-    .catch(err => {
-      console.error(err);
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
     });
-
-  app.listen(PORT, function () {
-    console.info(`Server listening on ${this.address().port}`);
-  }).on('error', err => {
-    console.error(err);
   });
 }
 
-module.exports = app; 
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
